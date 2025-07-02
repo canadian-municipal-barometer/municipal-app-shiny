8f9789e4b06994287b6a8a2876dc00a421f797e3
Reed <reedjmerrill@gmail.com>
2025-07-02 15:47:28 -0600

    added map and branding to UI

--- app.R
+++ app.R
@@ -3,11 +3,34 @@
 
 library(shiny)
 
-issues <- readRDS("data/issues.RDS")
+issues <- jsonlite::fromJSON("data/statements_en.json")
 
 municipal_policy_app <- function() {
   ui <- fluidPage(
-    map_ui("map", issues)
+    div(
+      id = "header",
+      style = "
+          display: flex;
+          align-items: center;
+          justify-content: space-around;
+          height: 150px;
+        ",
+      titlePanel("Municipal-level Policy Agreement"),
+      a(
+        img(
+          src = "https://www.cmb-bmc.ca/wp-content/uploads/2024/09/logo-bmc-cmb.svg" # nolint
+        ),
+        href = "https://www.cmb-bmc.ca/"
+      ),
+    ),
+    div(
+      style = "
+          display: flex;
+          justify-content: center;
+        ",
+      map_ui("map", issues)[[1]],
+    ),
+    map_ui("map", issues)[[2]]
   )
   server <- function(input, output, session) {
     map_server("map")
