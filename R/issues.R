fa250a6c044cb530b8ff9cd41284c6e04ac20406
Reed <reedjmerrill@gmail.com>
2025-07-14 15:43:52 -0600

    fix icon duplication in issues tab

--- R/issues.R
+++ R/issues.R
@@ -21,12 +21,12 @@ issues_server <- function(id, issue) {
             lengthChange = FALSE,
             headerCallback = JS(
               "function(thead, data, start, end, display) {",
-              "  var tooltips = ['', 'Full statement text.', 'Proportion that agree', 'Proportion that have an opinion', 'Standard deviation of municipal agreement proportions'];",
+              "  var tooltips = ['Full statement text', 'Proportion that agree', 'Proportion that have an opinion', 'Standard deviation of municipal agreement proportions'];",
               "  $(thead).find('th').each(function(i) {",
-              "    if (tooltips[i]) {",
-              "      var icon = $('<span> &#9432;</span>');",
+              "    if (tooltips[i] && $(this).find('.tooltip-icon').length === 0) {",
+              "      var icon = $('<span class=\"tooltip-icon\"> &#9432;</span>');",
               "      $(this).append(icon);",
-              "      $(this).find('span').attr('title', tooltips[i]);",
+              "      $(this).find('span.tooltip-icon').attr('title', tooltips[i]);",
               "    }",
               "  });",
               "}"
