ed7e695c3bac31c1680b7fda96a165e0e7a2ce7f
Reed <reedjmerrill@gmail.com>
2025-07-15 16:25:27 -0600

    details tab v1

--- R/details.R
+++ R/details.R
@@ -3,10 +3,42 @@ library(dplyr)
 library(ggplot2)
 library(DT)
 
+muni_data <- read.csv("data/municipal-data_raw.csv", check.names = FALSE)
+
+# add an empty first row so that the select input's selectize has the empty
+# string in its first position to enable placeholder prompt
+# These values are set as placeholders. 1 matches numeric columns in the
+# main `muni_data` and "" matches character. This enables the correct types
+# for the bind_rows operation
+muni_row1 <- tidyr::tibble(
+  csd = c(1),
+  prediction = c(1),
+  issue = c(""),
+  "Pct. Renters" = c(1),
+  Population = c(1),
+  "Pop. / sq. km" = c(1),
+  "Average Age" = c(1),
+  "Median After-tax income" = c(1),
+  Name = c(""),
+  Province = c("")
+)
+muni_data <- bind_rows(muni_row1, muni_data)
+muni_data$Population <- log(muni_data$Population)
+muni_data$"Pop. / sq. km" <- log(muni_data$"Pop. / sq. km")
+
 details_ui <- function(id) {
   tagList(
     "muni_menu" = uiOutput(NS(id, "muni_menu")),
-    "corr_menu" = uiOutput(NS(id, "corr_menu")),
+    "corr_menu" = selectInput(
+      NS(id, "corr_menu"),
+      label = "Correlate with...",
+      choices = colnames(muni_data)[
+        !(colnames(muni_data) %in%
+          c("prediction", "Name", "Province", "issue", "csd"))
+      ],
+      selectize = TRUE,
+      width = "100%",
+    ),
     "histogram" = plotOutput(NS(id, "histogram")),
     "corr_plot" = plotOutput(NS(id, "corr_plot")),
     "nat_comp" = plotOutput(NS(id, "nat_comp"))
@@ -15,30 +47,8 @@ details_ui <- function(id) {
 
 details_server <- function(id, issue) {
   moduleServer(id, function(input, output, session) {
-    muni_data <- read.csv("data/municipal-data_raw.csv", check.names = FALSE)
-
     # --------------------
     # municipality menu
-
-    # add an empty first row so that the select input's selectize has the empty
-    # string in its first position to enable placeholder prompt
-    # These values are set as placeholders. 1 matches numeric columns in the
-    # main `muni_data` and "" matches character. This enables the correct types
-    # for the bind_rows operation
-    muni_row1 <- tidyr::tibble(
-      csd = c(1),
-      prediction = c(1),
-      issue = c(""),
-      "Pct. Renters" = c(1),
-      Population = c(1),
-      "Pop. / sq. km" = c(1),
-      "Average Age" = c(1),
-      "Median After-tax income" = c(1),
-      Name = c(""),
-      Province = c("")
-    )
-    muni_data <- bind_rows(muni_row1, muni_data)
-    print(colnames(muni_data))
 
     output$muni_menu <- renderUI(
       selectInput(
@@ -91,7 +101,7 @@ details_server <- function(id, issue) {
     # --------------------
     # Correlation Plot
     output$corr_plot <- renderPlot({
-      req(input$corr_var)
+      req(input$corr_menu)
 
       corr_data <- muni_data |>
         filter(
@@ -103,13 +113,13 @@ details_server <- function(id, issue) {
       ggplot(
         corr_data,
         aes(
-          x = .data[[input$corr_var]],
+          x = .data[[input$corr_menu]],
           y = as.numeric(prediction)
         )
       ) +
         geom_point(alpha = 0.5) +
         geom_smooth(method = "lm", se = FALSE, color = "red") +
-        xlab(input$corr_var) +
+        xlab(input$corr_menu) +
         ylab("Pct. Agreement") +
         theme_minimal(base_size = 16)
     })
