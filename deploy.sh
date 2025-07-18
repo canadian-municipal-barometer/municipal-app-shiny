ea228f9a6945183e9bc2febae42572cd822f698d
Reed <reedjmerrill@gmail.com>
2025-07-18 08:35:05 -0600

    make file deletion in deploy.sh actually happen

--- deploy.sh
+++ deploy.sh
@@ -12,10 +12,10 @@ git checkout prod
 git merge main --no-ff -m "Merge main into prod for deployment"
 
 # Remove renv files
-git rm -rf renv .Rprofile renv.lock deploy.sh
+rm -rf renv .Rprofile renv.lock deploy.sh
 
 # Commit the changes
-git commit -m "Remove renv files for production deployment"
+git add -A && git commit -m "Remove renv files for production deployment"
 
 # Create the manifest file
 Rscript -e "rsconnect::writeManifest()"
