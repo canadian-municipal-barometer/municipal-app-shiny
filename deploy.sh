de0c7d2661b5bfd58b82a61ee4b07017443a8c0d
Reed <reedjmerrill@gmail.com>
2025-07-18 08:43:32 -0600

    commit renv changes in deploy.sh before merging

--- deploy.sh
+++ deploy.sh
@@ -4,22 +4,14 @@
 
 # update the renv snapshot
 Rscript -e "renv::snapshot()"
-
-# Switch to the 'prod' branch
+git add -A && git commit -m "update renv before deployment"
 git checkout prod
-
-# Merge changes from 'main'
 git merge main --no-ff -m "Merge main into prod for deployment"
-
-# Remove renv files
+# Remove renv files from prod (required for Posit Connect Cloud)
 rm -rf renv .Rprofile renv.lock deploy.sh
-
-# Commit the changes
 git add -A && git commit -m "Remove renv files for production deployment"
-
-# Create the manifest file
+# Create the manifest file (required for Posit Connect Cloud)
 Rscript -e "rsconnect::writeManifest()"
-
 echo "manifest.json updated"
 echo "'prod' branch is now ready for deployment"
 echo "Push 'prod' to deploy"
