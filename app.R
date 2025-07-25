03670d5fefc4d8d55efbe8144c23282ee4e994ec
Reed <reedjmerrill@gmail.com>
2025-07-25 14:30:50 -0600

    un-wrap app.R

--- app.R
+++ app.R
@@ -7,39 +7,38 @@ library(DT)
 
 issues <- jsonlite::fromJSON("data/statements_en.json")
 
-municipal_policy_app <- function() {
-  ui <- page_fillable(
-    padding = 0,
+ui <- page_fillable(
+  padding = 0,
+  div(
+    style = "height: 100vh;",
+    map_ui("map")["map"]
+  ),
+  absolutePanel(
+    top = "1rem",
+    right = "1rem",
+    width = "100px",
+    style = "z-index: 10;",
+    a(
+      img(
+        src = "main-logo-no-text.png",
+        style = "width: 100%; height: auto;"
+      ),
+      href = "https://www.cmb-bmc.ca/"
+    )
+  ),
+  absolutePanel(
+    top = "1rem",
+    left = "50%",
+    width = "600px",
+    style = "transform: translateX(-50%); z-index: 10;",
     div(
-      style = "height: 100vh;",
-      map_ui("map")["map"]
-    ),
-    absolutePanel(
-      top = "1rem",
-      right = "1rem",
-      width = "100px",
-      style = "z-index: 10;",
-      a(
-        img(
-          src = "main-logo-no-text.png",
-          style = "width: 100%; height: auto;"
-        ),
-        href = "https://www.cmb-bmc.ca/"
-      )
-    ),
-    absolutePanel(
-      top = "1rem",
-      left = "50%",
-      width = "600px",
-      style = "transform: translateX(-50%); z-index: 10;",
-      div(
-        style = "
+      style = "
           display: flex;
           flex-direction: column;
           align-items: center;
         ",
-        p(
-          style = "
+      p(
+        style = "
             position: relative;
             bottom: -37px;
             width: fit-content;
@@ -48,55 +47,51 @@ municipal_policy_app <- function() {
             text-align: center;
             background-color: white;
           ",
-          "Select an issue:"
-        ),
+        "Select an issue:"
       ),
-      selectInput(
-        "selected_issue",
-        label = "",
-        choices = issues,
-        width = "100%"
-      )
     ),
-    absolutePanel(
-      bottom = "1rem",
-      left = "1rem",
-      card(
-        gradientUI("grad")
-      )
+    selectInput(
+      "selected_issue",
+      label = "",
+      choices = issues,
+      width = "100%"
+    )
+  ),
+  absolutePanel(
+    bottom = "1rem",
+    left = "1rem",
+    card(
+      gradientUI("grad")
     )
   )
-  server <- function(input, output, session) {
-    map_server(
-      "map",
-      selected_issue = reactive({
-        input$selected_issue
-      })
-    )
-    issues_server("issues")
-    table_server(
-      "table",
-      selected_issue = reactive({
-        input$selected_issue
-      })
-    )
-    selected_muni <- details_server(
-      "details",
-      selected_issue = reactive({
-        input$selected_issue
-      })
-    )
-    natl_comp_server(
-      "natl_comp",
-      selected_issue = reactive({
-        input$selected_issue
-      }),
-      selected_muni = selected_muni
-    )
-    gradientServer("grad")
-  }
-  shinyApp(ui, server) # nolint
-  # profvis::profvis(runApp(shinyApp(ui, server))) #nolint
+)
+server <- function(input, output, session) {
+  map_server(
+    "map",
+    selected_issue = reactive({
+      input$selected_issue
+    })
+  )
+  issues_server("issues")
+  table_server(
+    "table",
+    selected_issue = reactive({
+      input$selected_issue
+    })
+  )
+  selected_muni <- details_server(
+    "details",
+    selected_issue = reactive({
+      input$selected_issue
+    })
+  )
+  natl_comp_server(
+    "natl_comp",
+    selected_issue = reactive({
+      input$selected_issue
+    }),
+    selected_muni = selected_muni
+  )
+  gradientServer("grad")
 }
-
-municipal_policy_app()
+shinyApp(ui, server) # nolint
